631283cb24a9f954412f9bdb65a1cdc0eba865ca
Matt Haines <matt@electricimp.com>
2015-08-27 14:22:03 -0700

    OK - let's try this again

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var http = require('http');
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
 
 http.createServer(function (req, res) {
-  console.log(req);
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(process.env.PORT)
 
-  res.statusCode = 200;
-  res.end('OK');
-}).listen(process.env.PORT);
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
+
+handler.on('issues', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})
