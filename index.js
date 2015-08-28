95039235683e40485bd1eb3d3d94ce7f1069678a
Matt Haines <matt@electricimp.com>
2015-08-28 11:05:35 -0700

    Actually doing stuff now

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var request = require("request");
 var createHandler = require("github-webhook-handler");
 var handler = createHandler({ path: "/webhook", secret: "myhashsecret" });
 
+// Set BUILD_API_KEY with: `heroku config:set BUILD_API_KEY=YourApiKey`
+var Imp = require("imp-api");
+var imp = Imp({ apiKey: process.env.BUILD_API_KEY });
+
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
@@ -22,14 +26,76 @@ handler.on("push", function (event) {
   if(isPrivate || ref !== "refs/heads/master") return;
 
   var configUrl = "https://raw.githubusercontent.com/" + repo + "/master/.impconfig";
-  console.log(configUrl);
 
-  request(configUrl, function(err, resp, body) {
-    console.log(resp);
+  request(configUrl, function(err, resp, impconfig) {
     // If .impconfig isn't present, there isn't anything we can do.
+    if(resp.statusCode != 200) {
+      console.log("Couldn't find .impconfig in root level of repository";
+      return;
+    }
 
-    if(resp.statusCode != 200) return;
+    // Make sure have the required data
+    if (!impconfig.modelId || !impconfig.deviceFile || !impconfig.agentFile) {
+      console.log("Invalid .impconfig - ensure modelId, deviceFile, and agentFile are present");
+      return;
+    }
 
-    console.log(body);
+    // Fetch the code
+    var deviceFileUrl = "https://raw.githubusercontent.com/" + repo + "/master/" + impconfig.deviceFile;
+    var agentFileUrl = "https://raw.githubusercontent.com/" + repo + "/master/" + impconfig.agentFile;
+
+    request(deviceFileUrl, function(deviceErr, deviceResp, deviceCode) {
+      if (deviceResp.statusCode != 200) {
+        console.log("Error fetching device file: " + impconfig.deviceFile);
+        return;
+      }
+
+      request(agentFileUrl, function(agentErr, agentRest, agentCode) {
+        if (agentResp.statusCode != 200) {
+          console.log("Error fetching agent file: " + impconfig.agentFile);
+          return;
+        }
+
+        var model = {
+          device_code: deviceCode,
+          agent_code: agentCode
+        };
+
+        imp.createModelRevision(impconfig.modelId, model, function(err, data) {
+          if (err) {
+            if (err.code != "CompileFailed") {
+              console.log(colors.red("ERROR: " + err.message_short));
+              return;
+            }
+
+            if (err.details.agent_errors) {
+              for(var i = 0; i < err.details.agent_errors.length; i ++) {
+                var thisErr = err.details.agent_errors[i];
+                console.log(colors.red("ERROR: " + thisErr.error));
+                console.log("   at: " + config.get("agentFile") +":" + thisErr.row + " (col "+thisErr.column+")");
+              }
+            }
+
+            if (err.details.device_errors) {
+              for(var i = 0; i < err.details.device_errors.length; i ++) {
+                var thisErr = err.details.device_errors[i];
+                console.log(colors.red("ERROR: " + thisErr.error));
+                console.log("   at: " + config.get("deviceFile") +":" + thisErr.row + " (col "+thisErr.column+")");
+              }
+            }
+
+            return;
+          }
+
+          imp.restartModel(config.get("modelId"), function(err, restartData) {
+            if (err) {
+              console.log("Warning: Could not restart model");
+            }
+
+            console.log("Successfully created revision " + data.revision.version);
+          });
+        });
+      });
+    });
   });
 });
